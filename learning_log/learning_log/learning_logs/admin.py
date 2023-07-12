a4e11014bc8658ad21548a2d6f22d4db40fce397
Stepan Cherkashin <120642009+stchstepan@users.noreply.github.com>
2023-07-12 23:24:46 +0300

    Delete learning_log/learning_log directory

--- learning_log/learning_log/learning_logs/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Topic, Entry #импортируем модель из models.py
-#. (точка) перед models сообщает Django, что модель следует 
-#искать в одном каталоге с admin.py
-
-admin.site.register(Topic) #данный вызов сообщает Django, что
-#управление моделью должно осуществляться через административный
-#сайт
-admin.site.register(Entry)
